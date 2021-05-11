e1f0594f81cd57eaf47dc407e8507be6bafa689b
HannaBM12 <Hannabmulugeta@gmail.com>
2021-05-11 15:02:20 -0400

    Sorting Login issue

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+    skip_before_action :authenticate, only: [:index, :show]
 
     def index
         trips = Trip.all
@@ -6,12 +7,21 @@ class TripsController < ApplicationController
 
     end
 
+    # def show
+    #     trip = Trip.find_by(id: params[:id])
+    #     render json: trip, except:[:created_at, :updated_at]
+    # end
+
     def show
-        trip = Trip.find_by(id: params[:id])
-        render json: trip, except:[:created_at, :updated_at]
+        trips = Trip.where(client_id: params[:id])
+        render json: trips.to_json(:include => {
+            :attraction => {:only => [:name, :image]}
+        })
     end
 
     def create
+        trips = Trip.create!(trip_params)
+        render json: trips
 
     end
 
@@ -19,6 +29,7 @@ class TripsController < ApplicationController
     private
     
     def trip_params
+        params.permit(:date, :quantity, :reviews, :rating, :total, :attraction_id, :client_id)
 
     end
 
