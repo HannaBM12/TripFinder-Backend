acb7b3c4ed436097f1f16c25699542e6b3af4bed
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-05-12 17:39:36 -0400

    working update

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-    skip_before_action :authenticate, only: [:index, :show]
+    skip_before_action :authenticate, only: [:index, :show, :update]
     
 
     def index
@@ -20,23 +20,25 @@ class TripsController < ApplicationController
         })
     end
 
-    # def show
-    #     trips = Trip.where(client_id: params[:id])
-    #     render json: trips.to_json(:include => {
-    #         :attraction => {:only => [:name, :image]}
-    #     })
-    # end
-
 
    def create
         trips = Trip.create!(trip_params)
         render json: trips
 
     end
-
+    
+    def update
+        #  byebug
+        trip = Trip.find_by(id: params[:id])
+        trip.update!(rating: params[:rating], reviews: params[:reviews])
+        render json: trip
+    end
+#<ActionController::Parameters {"rating"=>"0.5", "reviews"=>"good", "controller"=>"trips", "action"=>"update", "id"=>"8", "trip"=>{"reviews"=>"good", "rating"=>"0.5"}} permitted: false>
 
     def destroy
+        # byebug
         trip = Trip.find(params[:id])
+       
         trip.destroy!
 
         render json: {}
@@ -47,6 +49,10 @@ class TripsController < ApplicationController
     def trip_params
         params.permit(:date, :quantity, :reviews, :rating, :total, :attraction_id, :client_id)
 
+    end
+
+    def update_trip_params
+        params.permit()
     end
 
 
